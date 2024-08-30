21dd1d68468c92f24205b755a766668cc0b2ac92
Hrushi1227 <rushi.ghatol@gmail.com>
2024-08-31 00:01:53 +0530

    navigation method hooks demos

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,14 +1,21 @@
 "use client";
-import { useRouter, useParams, useSearchParams } from "next/navigation";
+import {
+  useRouter,
+  useParams,
+  useSearchParams,
+  usePathname,
+} from "next/navigation";
 
 const PropertyPage = () => {
   const router = useRouter();
   const params = useParams();
   const searchParams = useSearchParams();
+  const pathname = usePathname();
 
   console.log("Check router ", router);
   return (
     <div>
+      <div> Property Path Name is {pathname}</div>
       <div> Property Page search Param is {searchParams.get("name")}</div>
       <div> Property Page Id is {params.id}</div>
       <button onClick={() => router.replace("/")}> Click Me</button>
