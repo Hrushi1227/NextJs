aef5796c0e3083a3ba96c268475a775aad38ca2f
Hrushi1227 <rushi.ghatol@gmail.com>
2024-08-30 22:52:45 +0530

    images and icon

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,11 @@
 import '@/assets/styles/globals.css';
 
+export const metadata = {
+  title : 'next js property plus',
+  keywords : 'rental property ',
+  description : 'find the perfect property'
+}
+
 const MainLayout = ({ children }) => {
   return (
     <html>
