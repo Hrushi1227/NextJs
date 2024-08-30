9b563a3fccfb0f1c172d5db17b9808f3b4f54933
Hrushi1227 <rushi.ghatol@gmail.com>
2024-08-31 00:28:58 +0530

    Navigatin and desining

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,17 @@
-import '@/assets/styles/globals.css';
+import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
-  title : 'next js property plus',
-  keywords : 'rental property ',
-  description : 'find the perfect property'
-}
+  title: "next js property plus",
+  keywords: "rental property ",
+  description: "find the perfect property",
+};
 
 const MainLayout = ({ children }) => {
   return (
     <html>
       <body>
+        <Navbar />
         <main>{children}</main>
       </body>
     </html>
--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,26 +1,5 @@
-"use client";
-import {
-  useRouter,
-  useParams,
-  useSearchParams,
-  usePathname,
-} from "next/navigation";
-
-const PropertyPage = () => {
-  const router = useRouter();
-  const params = useParams();
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-
-  console.log("Check router ", router);
-  return (
-    <div>
-      <div> Property Path Name is {pathname}</div>
-      <div> Property Page search Param is {searchParams.get("name")}</div>
-      <div> Property Page Id is {params.id}</div>
-      <button onClick={() => router.replace("/")}> Click Me</button>
-    </div>
-  );
+const PropertyPage = ({ params }) => {
+  return <div>Property Page Id {params.id}</div>;
 };
 
 export default PropertyPage;
