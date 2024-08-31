65508c22a990156e71e247f30d31a1054f0b1324
Hrushi1227 <rushi.ghatol@gmail.com>
2024-08-31 16:23:11 +0530

    added property and home properties from json as of now

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,5 +1,29 @@
+import PropertyCard from "@/components/PropertyCard";
+import properties from "@/properties.json";
+
 const PropertiesPage = () => {
-    return ( <div>Property page</div> );
-}
- 
+  return (
+    <section>
+      <div className="px-4 py-6">
+        <div className="container-x1 lg:container m-auto px-4 py-6">
+          {properties.length === 0 ? (
+            <p>No Properties found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property) => {
+                return (
+                  <PropertyCard
+                    key={property._id}
+                    property={property}
+                  ></PropertyCard>
+                );
+              })}
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default PropertiesPage;
